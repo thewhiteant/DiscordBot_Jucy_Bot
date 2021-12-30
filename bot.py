19028c2d9b4df43c41a12eee08565847cea52738
White Ant <trafi227@gmail.com>
2021-12-30 19:15:40 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -381,59 +381,59 @@ async def stop(ctx):
                     await ctx.voice_client.disconnect()
 
 
-@client.command(pass_context=True)
-async def gv(ctx, url):
-    class Fbdl:
-        def __init__(self):
-            self.req = requests.Session()
-            self.banner()
-        def banner(self):
-            ur = url
-            rl = ur.replace('https://m.', 'https://mbasic.').replace('https://www.', 'https://mbasic.')
-            self.getlnk(rl)
-        def getlnk(self, url):
-            r = self.req.get(url)
-            rr = re.findall(r'<a href="(.*?)"', r.text)
-            all_video = []
-            for x in rr:
-                if "/video_redirect/?src=" in x:
-                    all_video.append(x)
-            data = all_video[0]
-            self.dl(data)
+# @client.command(pass_context=True)
+# async def gv(ctx, url):
+#     class Fbdl:
+#         def __init__(self):
+#             self.req = requests.Session()
+#             self.banner()
+#         def banner(self):
+#             ur = url
+#             rl = ur.replace('https://m.', 'https://mbasic.').replace('https://www.', 'https://mbasic.')
+#             self.getlnk(rl)
+#         def getlnk(self, url):
+#             r = self.req.get(url)
+#             rr = re.findall(r'<a href="(.*?)"', r.text)
+#             all_video = []
+#             for x in rr:
+#                 if "/video_redirect/?src=" in x:
+#                     all_video.append(x)
+#             data = all_video[0]
+#             self.dl(data)
 
-        def dl(self, link):
-            re = link.replace('/video_redirect/?src=', '')
-            ree = urllib.parse.unquote(re)
-            with open(f"Test.mp4", "wb") as f:
-                response = requests.get(ree, stream=True)
-                total_length = response.headers.get('content-length')
-                if total_length is None:
-                        pass
-                else:
-                    dlw = 0
-                    total_length = int(total_length)
-                    for data in response.iter_content(chunk_size=4096):
-                        ges = int(100*dlw/total_length)
-                        dlw += len(data)
+#         def dl(self, link):
+#             re = link.replace('/video_redirect/?src=', '')
+#             ree = urllib.parse.unquote(re)
+#             with open(f"Test.mp4", "wb") as f:
+#                 response = requests.get(ree, stream=True)
+#                 total_length = response.headers.get('content-length')
+#                 if total_length is None:
+#                         pass
+#                 else:
+#                     dlw = 0
+#                     total_length = int(total_length)
+#                     for data in response.iter_content(chunk_size=4096):
+#                         ges = int(100*dlw/total_length)
+#                         dlw += len(data)
 
-                        f.write(data)
-                        done = int(25*dlw/total_length)
-                        sys.stdout.write(f"\r[{'>'*done}{'='*(25-done)}] {ges+1}% ")
-                        sys.stdout.flush()
+#                         f.write(data)
+#                         done = int(25*dlw/total_length)
+#                         sys.stdout.write(f"\r[{'>'*done}{'='*(25-done)}] {ges+1}% ")
+#                         sys.stdout.flush()
 
 
-#  try:
-    Fbdl()
-    await ctx.channel.purge(limit=1)
-    msg = await ctx.send(file=discord.File("Test.mp4"))
-    if os.path.isfile('Test.mp4') == True:
-         os.remove("Test.mp4")
+#     try:
+#         Fbdl()
+#         await ctx.channel.purge(limit=1)
+#         await ctx.send(file=discord.File("Test.mp4"))
+#         if os.path.isfile('Test.mp4') == True:
+#             os.remove("Test.mp4")
 
-    # except:
-    #     await ctx.send("Video Is not found / Too Large!! 8MB limit")
-    #     await ctx.send(url)
-    #     if os.path.isfile('Test.mp4') == True:
-    #         os.remove("Test.mp4")
+#     except:
+#         await ctx.send("Video Is not found / Too Large!! 8MB limit")
+#         await ctx.send(url)
+#         if os.path.isfile('Test.mp4') == True:
+#             os.remove("Test.mp4")
         
     
 
