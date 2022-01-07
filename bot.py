18a4263484d0cf2de8dd0669adc810de857aebd2
White Ant <trafi227@gmail.com>
2022-01-07 20:50:08 +0600

    .

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from asyncio.tasks import sleep
 from datetime import date
 from math import inf
 import discord
+from discord import message
 from discord.ext import commands ,tasks
 import datetime
 from discord.utils import async_all, get
@@ -319,12 +320,6 @@ async def cl(ctx):
 
 
 
-
-
-
-
-
-
 #time is in 24hr format
  #channel ID to send images to
 
@@ -334,9 +329,9 @@ async def cl(ctx):
 async def time_check():
     await client.wait_until_ready()
     message_channel = client.get_channel(id=928508066192850986)
+    await message_channel.purge(limit=1)
     await message_channel.send(f"> **{quoteCOl()}**")
-  
-
 time_check.start()
+
 BOTT = "OTE5NTc0MDAxMzU1OTg0OTA2.YbXyBg.w5-iHGAyYq9405Dye3I7LTzS338"
 client.run(BOTT)
